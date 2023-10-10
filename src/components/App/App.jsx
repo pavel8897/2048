0a15f2f563b731dbaa0f45bafad7be58c2b4fbaf
Pave Nikiforov <pavel8897@bk.ru>
2023-10-10 18:01:24 +0300

    new

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,18 +3,15 @@ import Field from '../Field/Field.jsx'
 import './app.css';
 
 let gridSize = 4;
-let arr = [];
-for (let i = 1; i <= gridSize; i++) {
-    for (let j = 1; j <= gridSize; j++) {
-        arr.push({ row: i, col: j });
-    }
-}
+let arr = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));
 
 function App() {
     const [cells, setCells] = useState(arr);
 
     return (
-        <Field arr={cells} />
+        <div className="field">
+            <Field cells={cells} gridSize={gridSize} />
+        </div>        
     )
 }
 
--- src/components/Field/Field.jsx
+++ src/components/Field/Field.jsx
@@ -1,26 +1,46 @@
-import React, { useState } from 'react';
-import Box from '../Box/Box.jsx';
+import React, { useState, useEffect } from 'react';
 import './field.css';
 
-function Field({arr}) {
-    console.log(arr)
+function Field({cells, gridSize}) {
     function getRandomCell() {
-        let emptyArr = []
-        arr.forEach(item => {
-            if(!item.count) {
-                emptyArr.push(item)
+        let emptyArr = [];
+        for(let i = 0; i < gridSize; i++) {
+            for(let j = 0; j < gridSize; j++) {
+                if(!cells[i][j]) {
+                    emptyArr.push({row: i, col: j});
+                }
             }
-        })
-        return emptyArr[Math.floor(Math.random() * (emptyArr.length))]
+        }
+        if(cells.length == 0) return null;
+        return emptyArr[Math.floor(Math.random() * (emptyArr.length))];
     }
     
-    getRandomCell()
+    function addCount() {
+        let cell = getRandomCell();
+        if(cell) {
+            cells[cell.row][cell.col] = Math.random() < 0.9 ? 2 : 4;
+        }
+        return cells;
+    }
 
-    return (
-        <div className="field">
-            
-        </div>
-    )
+    function outField() {
+        let field = document.querySelector('.field');
+        for (let i = 0; i < gridSize; i++) {
+          for (let j = 0; j < gridSize; j++) {
+            let tile = cells[i][j];
+            let cell = document.createElement('div');              
+            cell.classList.add('box');
+            cell.innerHTML = tile ? tile : '';
+            field.appendChild(cell);
+          }
+        }
+    }
+
+    addCount();
+    
+    useEffect(() => {
+        outField();
+    }, []);
 }
 
 export default Field;
